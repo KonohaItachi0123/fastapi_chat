e2348659a5b4f8bb72efa118ebf5c5f938d02c49
konohaitachi <konoha.itachi0123@gmail.com>
2023-04-09 18:17:29 -0700

    feat : add socket manager class

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from fastapi import (FastAPI, WebSocket, WebSocketDisconnect, Request)
 from pydantic import BaseModel
+from typing import List
 
 class Item(BaseModel):
     name : str
     description : str | None = None
     price : float
     tax : float | None = None
+
+class SocketManager:
+    def __init__(self):
+        self.active_connections: List[(WebSocket, str)] = []
+
+    async def connect(self, websocket: WebSocket, user: str):
+        await websocket.accept()
+        self.active_connections.append((websocket, user))
+
+    def disconnect(self, websocket: WebSocket, user: str):
+        self.active_connections.remove((websocket, user))
+
+    async def broadcast(self, data):
+        for connection in self.active_connections:
+            await connection[0].send_json(data)    
+
+manager = SocketManager()
 
 app = FastAPI()
 
@@ -19,7 +37,7 @@ async def create_item(item : Item) :
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World1112"}
+    return {"Hello1": "World1112"}
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: str, q:str | None = None):
